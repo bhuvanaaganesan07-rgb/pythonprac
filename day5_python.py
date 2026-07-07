9276b57fedb89ffd8f9229435f7657a350574ca7
bhuvana <bhuvanaaganesan07@gmail.com>
2026-07-07 20:58:45 +0530

    day5 created

---
+++ day5_python.py
@@ -0,0 +1,93 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=1
+y=3
+x+y
+4
+x_y
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    x_y
+NameError: name 'x_y' is not defined
+x-y
+-2
+x*y
+3
+x/y
+0.3333333333333333
+x%y
+1
+x=2
+x
+2
+x+=2
+
+x=x+2
+
+
+
+x=2
+x=x+2
+
+
+
+
+x
+4
+x+=2
+x
+6
+x-=1
+x
+5
+x*=6
+x
+30
+x\=7
+SyntaxError: unexpected character after line continuation character
+x/=8
+x
+3.75
+x=1
+y=2
+x==y
+False
+y=1
+x==y
+True
+x!=y
+False
+x
+1
+x=-x
+x
+-1
+x>y
+False
+x<y
+True
+x>=y
+False
+x<=y
+True
+x
+-1
+x=5
+9
+9
+y=9
+x>10 and y
+False
+x>10 and y<1
+False
+x>9 or y<4
+False
+y=not x
+y
+False
+x
+5
+x=True
+y = not x
+y
+False
