03f06129f01458bc961521f1f60826d36c5a6ad5
bhuvana <bhuvanaaganesan07@gmail.com>
2026-07-10 22:56:16 +0530

    day8 created

---
+++ day8_python.py
@@ -0,0 +1,33 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=[13,35,75,76,245,376,498,567]
+for i in a:
+    print(i)
+
+    
+13
+35
+75
+76
+245
+376
+498
+567
+
+for i in range(0,11)
+SyntaxError: expected ':'
+for i in range(0,11):
+    print(i)
+
+    
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
