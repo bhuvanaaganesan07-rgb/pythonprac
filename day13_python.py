530824e7466a114eccef10cc12adb04ff3c19657
bhuvana <bhuvanaaganesan07@gmail.com>
2026-07-22 22:01:00 +0530

    day13 created

---
+++ day13_python.py
@@ -0,0 +1,17 @@
+def ping():
+    return "ping!"
+x=ping()
+print(x)
+
+import math:
+    def volume(r):
+        v=(4.0 / 3.0) * math.pi * r ** 3
+        return v
+volume(3)
+
+def triangle_area(b,h):
+    return 0.5*b*h
+triangle_area(3,6)
+
+def g(y,x=0)
+    return x+y
