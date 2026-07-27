fa2f9fb9361548255e09dbe025357489faae1e25
bhuvana <bhuvanaaganesan07@gmail.com>
2026-07-27 20:50:40 +0530

    day1 created

---
+++ Day1.py
@@ -0,0 +1,37 @@
+
+x=3
+y=6
+
+type(x)
+<class 'int'>
+x
+3
+x+2
+5
+_+y
+11
+name="youtube"
+name
+'youtube'
+type(name)
+<class 'str'>
+name[2]
+'u'
+name[0:2]
+'yo'
+name[0:3]
+'you'
+name[4:]
+'ube'
+name[3:]
+'tube'
+'my'+_
+'mytube'
+_+'.com'
+'mytube.com'
+name
+'youtube'
+name[0]='y'
+
+len(name)
+7
