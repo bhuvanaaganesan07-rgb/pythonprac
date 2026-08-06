deaf13998deee348eebed03cbbfdd9f1e3002df0
bhuvana <bhuvanaaganesan07@gmail.com>
2026-08-06 22:00:16 +0530

    day1 created

---
+++ day1_profile.py
@@ -0,0 +1,12 @@
+print("===== My Profile=====")
+name="Bhuvana"
+college="PSNACET"
+branch="IT"
+year="3rd"
+goal="Placed at Zoho"
+print("Name:"+name)
+print("College:"+college)
+print("Branch:"+branch)
+print("Year:"+year)
+print("Goal:"+goal)
+print("====================")
