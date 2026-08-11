687ac58707e6c66e5d1ae8a5990d3808a5556215
bhuvana <bhuvanaaganesan07@gmail.com>
2026-08-11 22:41:52 +0530

    day2-python

---
+++ day2_python.py
@@ -0,0 +1,35 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=5
+type(x)
+<class 'int'>
+y=float(x)
+y
+5.0
+x=3
+y=2
+z=complex(x,y)
+z
+(3+2j)
+
+list=[1,2,3,4]
+type(list)
+<class 'list'>
+set={1,2,3}
+tuple=(1,2)
+type(tuple)
+<class 'tuple'>
+x=5
+x>y
+True
+x<y
+False
+d={1:"python",2:"java",3:"c++"}
+
+
+d.values()
+dict_values(['python', 'java', 'c++'])
+d.keys()
+dict_keys([1, 2, 3])
+d[2]
+'java'
